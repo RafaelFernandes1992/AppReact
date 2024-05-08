665b48f91e2bd027f59286472053e57ec8809cf2
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2024-05-07 21:59:33 -0300

    Update Contador.js

--- src/Components/Contador.js
+++ src/Components/Contador.js
@@ -22,61 +22,65 @@ function Contador() {
     return(
         <>
         <div class="container">
-            <div class="w-50 h-50 border border-black border-3 mx-auto mt-5 p-1">
-                
-                <div class="row ps-1">
-                    <div class="col-10 text-center">
-                        <h2 class="ms-5 ps-5">Total</h2>
-                    </div>
-                    <div class="col-2">
-                        <a href="#" onClick={zerar}>
-                            <img src="atualizar.svg" class="img-fluid" width="50" />
-                        </a>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="border border-black border-2 rounded w-25 mx-auto text-center h4">{Total}</div>
-                </div>
-
-                <div class="row pt-5 text-center">
-                    <div class="col-6">
-                        <img src="homem.png" class="mx-auto img-fluid w-40" />
-                    </div>
-                    <div class="col-6">
-                        <img src="mulher.png" class="mx-auto img-fluid w-40" />
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-
-                    <div class="col-3 text-center">
-                        <button type="button" class="btn btn-success" onClick={maisHomem}>+</button>
-                    </div>
-                    
-                    <div class="col-3 text-center">
-                        <button type="button" class="btn btn-danger" onClick={menosHomem}>-</button>
+            <div class="card w-50 h-50 mx-auto">
+                <div class="card-body"> 
+                    <div class="row ps-1">
+                        <div class="col-10 text-center">
+                            <h2 class="ms-5 ps-5">Total</h2>
+                        </div>
+                        <div class="col-2">
+                            <button type="button" class="btn btn-success" onClick={zerar}>Atualizar</button>
+                        </div>
                     </div>
 
-                    <div class="col-3 text-center">
-                        <button type="button" class="btn btn-success" onClick={maisMulher}>+</button>
+                    <div class="row">
+                        <div class="col-3"></div>
+                        <div class="col-6 text-center">
+                            <div class="alert alert-dark text-center" role="alert">{Total}</div>
+                        </div>
+                        <div class="col-3"></div>
                     </div>
 
-                    <div class="col-3 text-center">
-                        <button type="button" class="btn btn-danger" onClick={menosMulher}>-</button>
+                    <div class="row pt-5 text-center">
+                        <div class="col-6">
+                            <img src="homem.png" class="mx-auto img-fluid w-40" />
+                        </div>
+                        <div class="col-6">
+                            <img src="mulher.png" class="mx-auto img-fluid w-40" />
+                        </div>
                     </div>
 
-                </div>
+                    <div class="row mt-3">
 
+                        <div class="col-3 text-center">
+                            <button type="button" class="btn btn-success" onClick={maisHomem}>+</button>
+                        </div>
+                        
+                        <div class="col-3 text-center">
+                            <button type="button" class="btn btn-danger" onClick={menosHomem}>-</button>
+                        </div>
 
-                <div class="row mt-3">
-                    <div class="col-6">
-                        <h4 class="text-center">Homens</h4>
-                        <div class="border border-black rounded border-2 w-50 mx-auto text-center h4">{TotalHomem}</div>
+                        <div class="col-3 text-center">
+                            <button type="button" class="btn btn-success" onClick={maisMulher}>+</button>
+                        </div>
+
+                        <div class="col-3 text-center">
+                            <button type="button" class="btn btn-danger" onClick={menosMulher}>-</button>
+                        </div>
+
                     </div>
-                    <div class="col-6">
-                        <h4 class="text-center">Mulheres</h4>
-                        <div class="border border-black rounded border-2 w-50 mx-auto text-center h4">{TotalMulher}</div>
+
+
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <h4 class="text-center">Homens</h4>
+                            <div class="alert alert-dark text-center" role="alert">{TotalHomem}</div>
+
+                        </div>
+                        <div class="col-6">
+                            <h4 class="text-center">Mulheres</h4>
+                            <div class="alert alert-dark text-center" role="alert">{TotalMulher}</div>
+                        </div>
                     </div>
                 </div>
             </div>
