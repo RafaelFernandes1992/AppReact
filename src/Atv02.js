6b3a62203efa52716c8f17f927fbd06cfbfb1149
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2024-05-07 21:43:39 -0300

    Update Atv02.js

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,14 +1,20 @@
+import Contador from './Components/Contador';
 import { Link } from 'react-router-dom';
 
-export default function Atv01() {
+export default function Atv02() {
     return(
         <>
             <h1>Atividade 02</h1>
+            <br />
+            <p>
+                <Contador />
+            </p>
 
-            
             <br />
             <Link to="/">Retornar à página inicial</Link>
+            <br />
+            <br />
+            <p> <b>Aluno: </b>Rafael Fernandes de Melo Lopes</p>
         </>
     );
 }
-
