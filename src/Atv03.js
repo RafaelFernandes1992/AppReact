b8af872fcd7520dcd51f379ec94069828f984042
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2024-05-21 00:49:47 -0300

    atualização atividade 3

--- src/Atv03.js
+++ src/Atv03.js
@@ -1,5 +1,11 @@
-import Contador from './Components/Contador';
 import { Link } from 'react-router-dom';
+import { Profile } from './Components/Gallery';
+import Gallery from './Components/Gallery';
+import Avatar from './Components/Avatar';
+import TodoList from './Components/TodoList';
+import List from "./Components/List";
+
+
 
 export default function Atv03() {
     return(
@@ -7,7 +13,11 @@ export default function Atv03() {
             <h1>Atividade 03</h1>
             <br />
             <p>
-                teste
+                <Profile />
+                <Gallery />
+                <Avatar />
+                <TodoList />
+                <List />
             </p>
             <br />
             <Link to="/">Retornar à página inicial</Link>
