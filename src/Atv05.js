b714c03c1ccfa49d9ab9b8c5a685794b0a7c4e5e
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2024-06-04 16:34:06 -0300

    Add files via upload

--- src/Atv05.js
+++ src/Atv05.js
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
+import FormQuiz from './Components/Atv5_FormQuiz';
+import FormTicket from './Components/Atv5_FormTicket';
+import Accordion from './Components/Atv5_Accordion';
+import Messenger from './Components/Atv5_App';
 
 export default function Atv05() {
     return(
         <>
-            <h1>Atividade 04</h1>
+            <h1>Atividade 05</h1>
             <br />
             <p>
-
+                <FormQuiz />
+                <FormTicket />
+                <Accordion />
+                <Messenger />
+                
             </p>
             <br />
             <Link to="/">Retornar à página inicial</Link>
--- src/Atv06.js
+++ src/Atv06.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function Atv06() {
     return(
         <>
-            <h1>Atividade 04</h1>
+            <h1>Atividade 06</h1>
             <br />
             <p>
-
+            O Carrinho de Compras não será publicada no Vercel pois possui backend e BD além do front em React. Será apresentado em aula!
             </p>
             <br />
             <Link to="/">Retornar à página inicial</Link>
